11879f0bf1cd2e5ee940f3f2251f005492c5b5d9
DenisZ <zayczev2012@mail.ru>
2021-11-14 19:32:17 +0500

    HeaderFinal

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,20 @@
 <template>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<nav class="navbar navbar-expand-lg navbar-light">
   <div class="container">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <a class="navbar-brand logo" href="/"><img src="../assets/images/icons/logo.png" alt="logo"></a>
+    <div class="collapse collapse-fontsize navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item" v-for="(menu, index) in headerMenuList" :key="index">
+        <li class="nav-item menu-link" v-for="(menu, index) in headerMenuList" :key="index">
           <router-link class="nav-link active" aria-current="page" :to="menu.path">
 						{{ menu.name }}
 						</router-link>
         </li>
       </ul>
-			<div>
-				<a href=""><img src="../assets/images/icons/f.svg" alt=""></a>
-				<a href=""><img src="../assets/images/icons/p.svg" alt=""></a>
-				<a href=""><img src="../assets/images/icons/instagram.svg" alt=""></a>
-				<a href=""><img src="../assets/images/icons/google.svg" alt=""></a>
+			<div class="icons">
+				<a href="https://www.facebook.com/" target="_blank"><img src="../assets/images/icons/f.svg" alt=""></a>
+				<a href="https://www.pinterest.ru/" target="_blank"><img src="../assets/images/icons/p.svg" alt=""></a>
+				<a href="https://www.instagram.com/" target="_blank"><img src="../assets/images/icons/instagram.svg" alt=""></a>
+				<a href="https://www.google.com/account/about/" target="_blank"><img src="../assets/images/icons/google.svg" alt=""></a>
 			</div>
     </div>
   </div>
@@ -41,21 +41,38 @@
 	}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only
-  // <style scoped lang="scss"> 
-// 	h3 {
-// 		margin: 40px 0 0;
-// 	}
-// 	ul {
-// 		list-style-type: none;
-// 		padding: 0;
-// 	}
-// 	li {
-// 		display: inline-block;
-// 		margin: 0 10px;
-// 	}
-// 	a {
-// 		color: #42b983;
-// 	}
-// </style>
--->
+
+<style  lang="scss"> 
+@import '../assets/layouts/index';
+	.navbar {
+		height: 90px;
+	}
+	.navbar-nav{
+		height: 90px;
+	}
+	.logo {
+		padding-right: 180px;
+	}
+	.collapse-fontsize{
+		font-size: 18px;
+	}
+	.menu-link{
+		padding: 25px 30px;
+
+		&:hover {
+			background:$gray;
+		}
+	}
+
+	.icons{
+		a {
+			width: 30px;
+			height: 30px;
+			text-align: center;
+			border: 1px solid $gray;
+			border-radius: 50%;
+			margin-right: 10px;
+			display: inline-block;
+		}
+	}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -10,10 +10,22 @@ const routes = [
 	{
 		path: '/about',
 		name: 'About',
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
 		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+	},
+	{
+		path: '/property',
+		name: 'Property',
+		component: () => import(/* webpackChunkName: "Property" */ '../views/Property.vue'),
+	},
+	{
+		path: '/blog',
+		name: 'Blog',
+		component: () => import(/* webpackChunkName: "Blog" */ '../views/Blog.vue'),
+	},
+	{
+		path: '/contacts',
+		name: 'Contacts',
+		component: () => import(/* webpackChunkName: "Contacts" */ '../views/Contacts.vue'),
 	},
 ]
 
